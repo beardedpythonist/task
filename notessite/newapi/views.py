f6c00c3b0133535e65729ffc395ed23e29b78fe4
Alexander Gordey <alexnickgordey3@gmail.com>
2023-03-15 12:42:45 +0300

    добавил роутеры и АПИ со всеми функциями!!!

--- notessite/newapi/views.py
+++ notessite/newapi/views.py
@@ -1,10 +1,11 @@
 from rest_framework.generics import ListCreateAPIView
 from main.models import Task
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
+
 from .serializer import TaskSerializer
 
 
-class PostViewSet(ModelViewSet):
+class PostViewSet(ReadOnlyModelViewSet):
     serializer_class = TaskSerializer
     queryset = Task.objects.all()
 
